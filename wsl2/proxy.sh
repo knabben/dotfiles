884a0783922906b212de70f230730c7c72fb557f
Amim Knabben <amim.knabben@gmail.com>
2024-09-06 14:22:00 -0300

    Adding golang proxy

---
+++ wsl2/proxy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+PROXY=proxy.golang.org
+sudo mkdir -p /usr/local/share/ca-certificates/extra
+true | openssl s_client -connect $PROXY:443 2>/dev/null | openssl x509 > $PROXY.pem
+sudo mv $PROXY.pem /usr/local/share/ca-certificates/extra/$PROXY.crt
+sudo update-ca-certificates
