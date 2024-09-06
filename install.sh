1850c8f406a4c607f3e63b2708901f6458b8ec04
Amim Knabben <amim.knabben@gmail.com>
2024-09-06 17:29:16 -0300

    Adding jq

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ USER=aknabben
 # Install default required packages
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get -y install tmux vim zsh make python3 python3-pip ipython fzf
+sudo apt-get -y install tmux vim zsh make python3 python3-pip ipython fzf jq
 
 # Set ZSH
 chsh -s /bin/zsh
